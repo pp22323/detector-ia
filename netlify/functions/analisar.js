8499218e04e62d63d8b6c31f40d480976ffcd7bc
pp22323 <pedronemetalas22@gmail.com>
2026-05-21 16:34:16 -0300

    Update analisar.js

--- netlify/functions/analisar.js
+++ netlify/functions/analisar.js
@@ -1,1 +1,20 @@
+exports.handler = async (event) => {
 
+return {
+
+statusCode:200,
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+status:"online",
+msg:"Detector pronto"
+
+})
+
+};
+
+};
