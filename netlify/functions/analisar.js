41649c0d8c9233d34e423bf9e9c52e8d67c83169
pp22323 <pedronemetalas22@gmail.com>
2026-05-21 16:48:23 -0300

    Update analisar.js

--- netlify/functions/analisar.js
+++ netlify/functions/analisar.js
@@ -1,40 +1,14 @@
-const axios = require("axios");
-
 exports.handler = async (event) => {
 
-try{
-
 return {
-
 statusCode:200,
-
 headers:{
 "Content-Type":"application/json"
 },
-
 body:JSON.stringify({
-
 status:"online",
-msg:"Detector backend ativo"
-
+msg:"Backend ativo"
 })
-
 };
-
-}catch(err){
-
-return {
-
-statusCode:500,
-
-body:JSON.stringify({
-
-erro:"Erro detector"
-
-})
-
-};
-
-}
 
 };
